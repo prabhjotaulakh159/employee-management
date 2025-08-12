76d8238ddf906fc746ded6bf6000c11c0551e467
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2025-08-12 15:57:54 -0400

    Create basic templates

---
+++ pages/index.php
@@ -0,0 +1,5 @@
+<?php include '../templates/header.php' ?>
+
+<h1>hello world</h1>
+
+<?php include '../templates/footer.php' ?>
---
+++ templates/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Employee Management</title>
+	</head>
+	<body>
+	
